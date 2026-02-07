b7ad05297d88043c749c02609685365d3ac1618a
Kali Builder <kali@local>
2026-02-07 13:04:20 -0600

    Modules and Firmware

--- device/motorola/hawao/BoardConfig.mk
+++ device/motorola/hawao/BoardConfig.mk
@@ -93,10 +93,12 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 BOARD_HAS_NO_SELECT_BUTTON := true
 
+
+TW_LOAD_VENDOR_MODULES := "mmi_annotate.ko mmi_info.ko sensors_class.ko mmi_relay.ko touchscreen_mmi.ko focaltech_v2_mmi.ko"
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
+#TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
